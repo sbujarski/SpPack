6c16d137b2998544ac8287c2622f910ca49314c7
sbujarski <s.bujarski@gmail.com>
2017-12-03 17:55:47 -0800

    Adding SpHist and SpDesc functions

---
+++ R/SpDesc.R
@@ -0,0 +1,57 @@
+#Displays summary statistics
+#Uses pastecs stat.desc for numeric data
+#prints tables for factor data
+#can take a vector or dataframe
+
+library(pastecs)
+
+SpDesc <- function(data)
+{
+  #If only 1 variable do separately 
+  if(dim(data.frame(data))[2]==1){
+    if(class(data)=="numeric"){#if R can compute a mean then stat.desc
+      print(t(stat.desc(data))[,-c(2,3,6,7,11,14)])
+    }
+    else{
+      #cat(noquote(levels(data)))
+      print(table(data))
+    }
+    return(noquote(""))
+  }
+  
+  #separate categorical from numerical data
+  data.num <- data.frame(Dummy=rep(NA,dim(data)[1])) #Make a dummy data.frame
+  data.cat <- data.frame(Dummy=rep(NA,dim(data)[1]))
+  
+  for(i in 1:dim(data)[2]){
+    if(!is.na(stat.desc(data[i])["mean",])){#if R can compute a mean then add to data.num
+      data.num <- cbind(data.num, data[i])
+    }
+    else{
+      data.cat <- cbind(data.cat, data[i])
+    }
+  }
+  #Delete dummy variable
+  data.num$Dummy <- NULL
+  data.cat$Dummy <- NULL
+  
+  #Print Numerical results
+  if(dim(data.num)[2]>0) {
+    print(t(stat.desc(data.num))[,-c(2,3,6,7,11,14)])
+    cat(noquote(""), sep="\n\n")
+  }
+  
+  #Print categorical results
+  if(dim(data.cat)[2]>0) {
+    for(j in 1:dim(data.cat)[2]){
+      cat(noquote(names(data.cat[j])))
+      print(table(data.cat[j]))
+    }
+  }
+}
+
+#Usage examples----
+# SpDesc(XYData)
+# SpDesc(XYData$X)
+# x3=c("A","A","B")
+# SpDesc(x3)
---
+++ R/SpHist.R
@@ -0,0 +1,44 @@
+#Generate Histogram using ggplot2 with overlayed normal distribution
+#Also includes mean and SD
+
+library(ggplot2)
+
+SpHist <- function(data, variable=NA, bins=30, save=F, text.xy)
+{
+  if(class(data)=="data.frame"){
+    data<-data[variable]
+    data<-na.exclude(data)
+    Mean<-mean(data[,1])
+    SD<-sd(data[,1])
+    
+    Histogram <- ggplot(data, aes(data[1])) + geom_histogram(aes(y=..density..), colour="white", bins=bins) + 
+      stat_function(fun=dnorm, args=list(mean=Mean, sd=SD), size=3) +
+      ggtitle(paste(variable, "Histogram", "\n", "Mean =", round(Mean,2), "SD =", round(SD,2))) + scale_x_continuous(variable) + SpTheme()
+  } else {
+    if(class(data)=="numeric"){
+      variable<-deparse(substitute(data))
+      data<-as.data.frame(data, col.names=T)
+      data<-na.exclude(data)
+      Mean<-mean(data[,1])
+      SD<-sd(data[,1])
+      
+      Histogram <- ggplot(data, aes(data)) + geom_histogram(aes(y=..density..), colour="white", bins=bins) + 
+        stat_function(fun=dnorm, args=list(mean=Mean, sd=SD), size=3) +
+        ggtitle(paste(variable, "Histogram", "\n", "Mean =", round(Mean,2), "SD =", round(SD,2))) + scale_x_continuous(variable) + SpTheme()
+    } else {
+      cat("-- ERROR --\n")  
+      cat("Please enter a numeric vector or a dataframe with numeric variable specified")
+      }
+    }
+    
+  if(save){
+    ggsave(Histogram, filename=paste(paste(variable, " Histogram.png", sep="")), width = 8, height=6.5, dpi=500)
+  }
+  return (Histogram)
+}
+
+#Usage examples----
+# SpHist(data=XYData, variable="X")
+# SpHist(data=x)
+# SpHist(data=XYData$Y)
+# SpHist(data=c("C","L","M")) -produces an error
