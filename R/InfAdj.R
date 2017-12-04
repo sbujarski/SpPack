4577109bee16cd6fc60a739bc5e5fe9d26080440
sbujarski <s.bujarski@gmail.com>
2017-12-03 17:58:50 -0800

    Updating Usage Examples

--- R/InfAdj.R
+++ R/InfAdj.R
@@ -5,32 +5,33 @@ InfAdj <- function(yr, adj.yr, value)
 {
   #import yearly CPI data
   yearly_cpi <- read.csv("C:/Users/sbuja/Documents/Statistics Misc/Yearly_CPIAUCSL.csv", header = TRUE)
-  
-  
+
+
   #if the adjusting for inflation is a scalar
   if(length(yr)==1){
     #calculate adjustment factor
     adj_factor <- yearly_cpi$cpi[yearly_cpi$Year==yr] / yearly_cpi$cpi[yearly_cpi$Year==adj.yr]
-  
+
     #adjust for inflation
     value.Infadj <- value / adj_factor
-  
+
     return(value.Infadj)
   } else { #If the adjustng for inflation is from a vector of yrs and values
     data=data.frame(yr=yr, value=value)
-    
+
     for(i in 1:dim(data)[1]){
       data$adj_factor[i] <- yearly_cpi$cpi[yearly_cpi$Year==data$yr[i]] / yearly_cpi$cpi[yearly_cpi$Year==adj.yr]
     }
-        
+
     data$value.Infadj <- data$value / data$adj_factor
-    
+
     return(data)
   }
 }
 
-#seems to work fine
-#InfAdj(yr=1990, adj.yr=2013, value=1)
-#test with multiple years and values
-#Dollars <- data.frame(yr=seq(1990,2010), value=1)
-#InfAdj(yr=Dollars$yr, adj.yr=2013, value=Dollars$value)
+#Usage examples
+# InfAdj(yr=1990, adj.yr=2013, value=1)
+#
+# test with multiple years and values
+# Dollars <- data.frame(yr=seq(1990,2010), value=1)
+# InfAdj(yr=Dollars$yr, adj.yr=2013, value=Dollars$value)
