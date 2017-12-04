469a2e812087798cad8b488a657bf44e69e5be3f
sbujarski <s.bujarski@gmail.com>
2017-12-03 19:26:52 -0800

    Adding Documentation

--- R/SimCorX.R
+++ R/SimCorX.R
@@ -1,16 +1,31 @@
-#Simulates correlated data with a given vector of x data
+#' Simulating Correlated Y for Given X
+#'
+#' Simulates bivariate correlation of specified n, means, sd, and rho
+#' @name SimCor
+#' @param n number of datapoints to simulate
+#' @param xmean mean of x variable
+#' @param xsd standard deviation of x variable
+#' @param ymean mean of y variable
+#' @param ysd standard deviation of y variable
+#' @param rho intended correlation
+#' @return dataframe of correlated x and y variables
+#' @keywords simulation
+#' @export
+#' @examples
+#' SimCor(n=20, xmean=10, xsd=5, ymean=1, ysd=.5, rho=.5)
+
 
 SimCorX <- function (x, ymean, ysd, rho)
 {
-  Xnorm <- (X-mean(X))/sd(X)
-  n <- length(X)
-  Ynorm <- rnorm(n,0,1)
+  xnorm <- (x-mean(x))/sd(x)
+  n <- length(x)
+  ynorm <- rnorm(n,0,1)
   a <- rho/(sqrt(1-rho^2))
-  Ycor <- a*Xnorm+Ynorm
+  ycor <- a*xnorm+ynorm
 
-  Y <- Ycor*Ysd+Ymean
+  y <- ycor*ysd+ymean
 
-  return (data.frame(X,Y))
+  return (data.frame(x,x))
 }
 
 #Usage Examples
