559952e8855d407893ba887126a6114c9ab746c9
sbujarski <s.bujarski@gmail.com>
2017-12-03 22:28:03 -0800

    Adding SpTheme documentation

--- R/SpTheme.R
+++ R/SpTheme.R
@@ -1,4 +1,20 @@
-#Spencer's default ggplot2 theme
+#' Spencer's Default Theme
+#'
+#' Default ggplot theme for Spencer's publications
+#' @name SpTheme
+#' @param axis.text.size axis text size (defaults to 12)
+#' @param axis.title.size axis title size (defaults to 12)
+#' @param title.size title size (degaults to 16)
+#' @param legend.position positions of the legend (defaults to none)
+#' @keywords plotting
+#' @export
+#' @examples
+#' Data <- data.frame(x=seq(1:10), y=rnorm(n=10))
+#' p <- ggplot(Data, aes(x=x, y=y))+ geom_point() +
+#'     SpTheme()
+#' print(p)
+#'
+#' #Spencer's default ggplot2 theme
 
 SpTheme <- function(axis.text.size=12, axis.title.size=12, title.size=16, legend.position="none")
 {
