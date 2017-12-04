dadd5ecee30980ac03e99caffd9200140e48cbd0
sbujarski <s.bujarski@gmail.com>
2017-12-03 17:56:14 -0800

    Adding SpTheme and DotRTheme functions

---
+++ R/DotRTheme.R
@@ -0,0 +1,11 @@
+#Data on the Rocks default theme
+
+DotRTheme <- function(axis.text.size=16, axis.title.size=16, title.size=20, legend.position="none")
+{
+  theme(panel.grid.major = element_line(colour="grey90"), panel.grid.minor = element_blank(), panel.background = element_blank(),
+        axis.line.x=element_line(colour="black"), axis.line.y=element_line(colour="black"),
+        axis.title.x=element_text(colour = "black", size=axis.title.size), axis.title.y=element_text(colour = "black", size=axis.title.size),
+        axis.text.x=element_text(colour = "black", size=axis.text.size), axis.text.y=element_text(colour = "black", size=axis.text.size),
+        plot.title=element_text(colour = "black",size=title.size, face="bold", hjust=.5),
+        axis.ticks=element_line(colour="black"), legend.position = legend.position, legend.key=element_blank())
+}
---
+++ R/SpTheme.R
@@ -0,0 +1,11 @@
+#Spencer's default ggplot2 theme
+
+SpTheme <- function(axis.text.size=12, axis.title.size=12, title.size=16, legend.position="none")
+{
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(),
+        axis.line.x=element_line(colour="black"), axis.line.y=element_line(colour="black"),
+        axis.title.x=element_text(colour = "black", size=axis.title.size), axis.title.y=element_text(colour = "black", size=axis.title.size),
+        axis.text.x=element_text(colour = "black", size=axis.text.size), axis.text.y=element_text(colour = "black", size=axis.text.size),
+        plot.title=element_text(colour = "black",size=title.size, face="bold", hjust=.5),
+        axis.ticks=element_line(colour="black"), legend.position = legend.position, legend.key=element_blank())
+}
