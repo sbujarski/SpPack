6007d4a116b0323134145baa7c6ff24c0e3449b6
sbujarski <s.bujarski@gmail.com>
2017-12-03 19:20:20 -0800

    Starting documentation and lower case x and y parameters

--- R/SimCor.R
+++ R/SimCor.R
@@ -1,16 +1,34 @@
+#' Simulating Correlated Data
+#'
+#' Simulates bivariate correlation of specified n, means, sd, and rho
+#' @name SimCor
+#' @param n number of datapoints to simulate
+#' @param n number fo datapoints to simulate
+#' @param n number fo datapoints to simulate
+#' @param n number fo datapoints to simulate
+
+#' @return dataframe adding adjustment factors and inflation adjusted values
+#' @keywords inflation
+#' @export
+#' @examples
+#' InfAdj(yr=1990, adj.yr=2013, value=1)
+#' Dollars <- data.frame(yr=seq(1990,2010), value=1)
+#' InfAdj(yr=Dollars$yr, adj.yr=2013, value=Dollars$value)
+
+
 #Simulates bivariate correlation of specified n, means, sd, and rho
 
-SimCor <- function (n, Xmean, Xsd, Ymean, Ysd, rho)
+SimCor <- function (n, xmean, xsd, ymean, ysd, rho)
 {
-  Xnorm <- rnorm(n,0,1)
-  Ynorm <- rnorm(n,0,1)
+  xnorm <- rnorm(n,0,1)
+  ynorm <- rnorm(n,0,1)
   a <- rho/(sqrt(1-rho^2))
-  Ycor <- a*Xnorm+Ynorm
-  
-  X <- Xnorm*Xsd+Xmean
-  Y <- Ycor*Ysd+Ymean
-  
-  return (data.frame(X,Y))
+  ycor <- a*xnorm+ynorm
+
+  x <- xnorm*xsd+xmean
+  y <- ycor*ysd+ymean
+
+  return (data.frame(x,y))
 }
 
 #Usage Examples
