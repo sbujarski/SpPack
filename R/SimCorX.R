1b2c760e56534e153fd3ed59f3e85d552a7ae3b8
sbujarski <s.bujarski@gmail.com>
2017-12-03 19:25:11 -0800

    lowercase x and y variables

--- R/SimCorX.R
+++ R/SimCorX.R
@@ -1,15 +1,15 @@
 #Simulates correlated data with a given vector of x data
 
-SimCorX <- function (X, Ymean, Ysd, rho)
+SimCorX <- function (x, ymean, ysd, rho)
 {
   Xnorm <- (X-mean(X))/sd(X)
   n <- length(X)
   Ynorm <- rnorm(n,0,1)
   a <- rho/(sqrt(1-rho^2))
   Ycor <- a*Xnorm+Ynorm
-  
+
   Y <- Ycor*Ysd+Ymean
-  
+
   return (data.frame(X,Y))
 }
 
