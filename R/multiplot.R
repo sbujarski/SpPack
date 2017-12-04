cc61e7fc178b41c193e6459588ba880bd7237833
sbujarski <s.bujarski@gmail.com>
2017-12-03 17:59:23 -0800

    Updating mutliplot usage examples

--- R/multiplot.R
+++ R/multiplot.R
@@ -1,12 +1,12 @@
 #Prints multiple plots in a grid
 
 multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
-  
+
   # Make a list from the ... arguments and plotlist
   plots <- c(list(...), plotlist)
-  
+
   numPlots = length(plots)
-  
+
   # If layout is NULL, then use 'cols' to determine layout
   if (is.null(layout)) {
     # Make the panel
@@ -15,24 +15,25 @@ multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
     layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
                      ncol = cols, nrow = ceiling(numPlots/cols))
   }
-  
+
   if (numPlots==1) {
     print(plots[[1]])
-    
+
   } else {
     # Set up the page
     grid.newpage()
     pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-    
+
     # Make each plot, in the correct location
     for (i in 1:numPlots) {
       # Get the i,j matrix positions of the regions that contain this subplot
       matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-      
+
       print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
                                       layout.pos.col = matchidx$col))
     }
   }
 }
 
-multiplot(xqqplot, xsdqqplot, yqqplot, ysdqqplot, cols=2)
+#Usage examples
+# multiplot(xqqplot, xsdqqplot, yqqplot, ysdqqplot, cols=2)
