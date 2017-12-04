b76e7f162aa2351a3c440cb8444126ad3b8537f0
sbujarski <s.bujarski@gmail.com>
2017-12-03 18:00:22 -0800

    Updating Usage examples

--- R/SpHist.R
+++ R/SpHist.R
@@ -10,8 +10,8 @@ SpHist <- function(data, variable=NA, bins=30, save=F, text.xy)
     data<-na.exclude(data)
     Mean<-mean(data[,1])
     SD<-sd(data[,1])
-    
-    Histogram <- ggplot(data, aes(data[1])) + geom_histogram(aes(y=..density..), colour="white", bins=bins) + 
+
+    Histogram <- ggplot(data, aes(data[1])) + geom_histogram(aes(y=..density..), colour="white", bins=bins) +
       stat_function(fun=dnorm, args=list(mean=Mean, sd=SD), size=3) +
       ggtitle(paste(variable, "Histogram", "\n", "Mean =", round(Mean,2), "SD =", round(SD,2))) + scale_x_continuous(variable) + SpTheme()
   } else {
@@ -21,23 +21,23 @@ SpHist <- function(data, variable=NA, bins=30, save=F, text.xy)
       data<-na.exclude(data)
       Mean<-mean(data[,1])
       SD<-sd(data[,1])
-      
-      Histogram <- ggplot(data, aes(data)) + geom_histogram(aes(y=..density..), colour="white", bins=bins) + 
+
+      Histogram <- ggplot(data, aes(data)) + geom_histogram(aes(y=..density..), colour="white", bins=bins) +
         stat_function(fun=dnorm, args=list(mean=Mean, sd=SD), size=3) +
         ggtitle(paste(variable, "Histogram", "\n", "Mean =", round(Mean,2), "SD =", round(SD,2))) + scale_x_continuous(variable) + SpTheme()
     } else {
-      cat("-- ERROR --\n")  
+      cat("-- ERROR --\n")
       cat("Please enter a numeric vector or a dataframe with numeric variable specified")
       }
     }
-    
+
   if(save){
     ggsave(Histogram, filename=paste(paste(variable, " Histogram.png", sep="")), width = 8, height=6.5, dpi=500)
   }
   return (Histogram)
 }
 
-#Usage examples----
+#Usage examples
 # SpHist(data=XYData, variable="X")
 # SpHist(data=x)
 # SpHist(data=XYData$Y)
