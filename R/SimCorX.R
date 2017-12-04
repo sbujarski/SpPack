c6d6aceb627842988d98a3ba67d7bf2cc1dc0951
sbujarski <s.bujarski@gmail.com>
2017-12-03 17:55:24 -0800

    Adding SimCorX function

---
+++ R/SimCorX.R
@@ -0,0 +1,17 @@
+#Simulates correlated data with a given vector of x data
+
+SimCorX <- function (X, Ymean, Ysd, rho)
+{
+  Xnorm <- (X-mean(X))/sd(X)
+  n <- length(X)
+  Ynorm <- rnorm(n,0,1)
+  a <- rho/(sqrt(1-rho^2))
+  Ycor <- a*Xnorm+Ynorm
+  
+  Y <- Ycor*Ysd+Ymean
+  
+  return (data.frame(X,Y))
+}
+
+#Usage Examples
+# SimCorX(x, Ymean=1, Ysd=.5, rho=.5)
